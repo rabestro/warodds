8f3d4ca54b278c357fe248535d523341640c71f5
Yegor Chemisov <yegor.chemisov@gmail.com>
2019-12-03 19:10:42 +0200

    WarOdds rewrited in NativeScript.Vue. Issue #7

---
+++ NativeScript.Vue/app/components/collect-statistics.js
@@ -0,0 +1,27 @@
+import simulateBattle from './simulate-battle';
+
+export default function collectStatistics(attackerArmy, defenderArmy, simulations) {
+    const attackerArmyMax = 8;
+    const defenderArmyMax = 32;
+    const attacker = [0, 0, 0, 0, 0, 0, 0, 0];
+    const defender = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var simulationsLeft = simulations;
+
+    do battle();
+    while (--simulationsLeft > 0);
+
+    return {
+        attacker: attacker.map(calculatePercent),
+        defender: defender.map(calculatePercent)
+    };
+
+    function battle() {
+        const battleResult = simulateBattle(attackerArmy, defenderArmy);
+        if (battleResult.isAttackerWin) attacker[attackerArmyMax - battleResult.attackersLeft]++;
+        else defender[defenderArmyMax - battleResult.defendersLeft]++;
+    }
+
+    function calculatePercent(number) {
+        return number / simulations;
+    }
+}
